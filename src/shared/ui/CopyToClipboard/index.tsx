f7cb05ea4d8acebbd69186c926f06ebd62887f9b
“sieugene” <eugenepoluakov@gmail.com>
2023-02-12 22:20:30 +0900

    chore : notification as portal

--- src/shared/ui/CopyToClipboard/index.tsx
+++ src/shared/ui/CopyToClipboard/index.tsx
@@ -3,6 +3,7 @@ import MuiAlert from '@mui/material/Alert'
 
 import Snackbar from '@mui/material/Snackbar'
 import React, { ComponentType, FC, forwardRef, useState } from 'react'
+import { createPortal } from 'react-dom'
 
 type Props = {
   children: React.ReactNode
@@ -39,18 +40,25 @@ export const CopyToClipboard: FC<Props> = ({
   const Wrapper = wrapper || Div
   return (
     <Wrapper onClick={() => onContainerClick && handleCopy()}>
-      <Snackbar
-        open={open}
-        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-        autoHideDuration={1500}
-        onClose={handleClose}
-      >
-        <Alert onClose={handleClose} severity='success' sx={{ width: '100%' }}>
-          Text was copied!
-        </Alert>
-      </Snackbar>
+      <Notification open={open} handleClose={handleClose} />
       <CopyBtn onDoubleClick={handleCopy}>{children}</CopyBtn>
     </Wrapper>
+  )
+}
+
+const Notification = ({ open, handleClose }: { open: boolean; handleClose: () => void }) => {
+  return createPortal(
+    <Snackbar
+      open={open}
+      anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      autoHideDuration={1500}
+      onClose={handleClose}
+    >
+      <Alert onClose={handleClose} severity='success' sx={{ width: '100%' }}>
+        Text was copied!
+      </Alert>
+    </Snackbar>,
+    document.body,
   )
 }
 
